8ebf2ac1e34d5f7709ccc38d6e6881a17a1ca158
Ashish <ashishkap157@gmail.com>
2025-04-20 17:05:23 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,9 @@
-// import path from "path";
-// import tailwindcss from "@tailwindcss/vite";
-// import react from "@vitejs/plugin-react";
-// import { defineConfig } from "vite";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-  
-// });
-
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -26,21 +11,38 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: "0.0.0.0",
-    port: Number(process.env.PORT) || 3000,
-    allowedHosts: [
-      "leadsourcing-frontend-production.up.railway.app",
-      "localhost",
-    ],
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: Number(process.env.PORT) || 3000,
-    allowedHosts: true, // <- Change to boolean true instead of string "all"
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  
 });
+
+
+
+// import path from "path";
+// import tailwindcss from "@tailwindcss/vite";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   server: {
+//     host: "0.0.0.0",
+//     port: Number(process.env.PORT) || 3000,
+//     allowedHosts: [
+//       "leadsourcing-frontend-production.up.railway.app",
+//       "localhost",
+//     ],
+//   },
+//   preview: {
+//     host: "0.0.0.0",
+//     port: Number(process.env.PORT) || 3000,
+//     allowedHosts: true, // <- Change to boolean true instead of string "all"
+//   },
+//   build: {
+//     outDir: "dist",
+//     emptyOutDir: true,
+//   },
+// });
